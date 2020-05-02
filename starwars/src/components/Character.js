3e076e00c2601cf33b823e75ccbcd064ed70da5a
Chris <chrisharwell72@gmail.com>
2020-05-02 13:43:07 -0400

    MVP Complete

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,39 @@
+import React, {useState} from 'react';
+import {Card, CardTitle, CardImg, CardText} from 'reactstrap';
+import axios from 'axios';
+
 // Write your Character component here
+
+const Character = (props) => {
+
+    const [char, setChar] = useState([]);
+    axios
+        .get('https://rickandmortyapi.com/api/character/')
+        .then(res => {
+            setChar(res.data.results);  
+        })
+    .catch(err => console.error('an error occurred between '
+    + 'lines 10 and 12 of Character.js',err));
+
+    const mapItem = char.map(item => {
+        return (
+            <div>
+                <Card body className="card">
+                    <CardTitle className='card-title'>{item.name}</CardTitle>
+                    <CardImg className='img-border' src={item.image}></CardImg>
+                    <CardText className='text-divider'>Status: {item.status}</CardText>
+                    <CardText className='text-divider'>Origin: {item.origin.name}</CardText>
+                    <CardText className='text-divider'>Species: {item.species}</CardText>
+                    <CardText className='text-divider'>Location: {item.location.name}</CardText>
+                    <CardText>Gender: {item.gender}</CardText>
+                </Card>
+
+            </div>
+        )
+    })
+    return(
+        <div>{mapItem}</div>
+    )
+}
+
+export default Character;
